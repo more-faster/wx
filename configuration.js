f057fbfce819469ab927649e21b7f6edf9d0a7c8
more-faster <62316920+more-faster@users.noreply.github.com>
2020-03-26 15:52:08 +0800

    Add files via upload

---
+++ configuration.js
@@ -0,0 +1,7 @@
+const urlPrefix ='https://free-api.heweather.net/s6/weather';
+
+const weatherkey ='4aee6daebf32437a86c8e7935119b6a2'
+module.exports ={
+  urlPrefix: urlPrefix,
+  weatherkey: weatherkey
+}
